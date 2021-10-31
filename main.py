497bd603b557d9ffea19761fd3a603b10c8cdeaf
kejlor <woj450@gmail.com>
2021-10-31 18:13:58 +0100

    Added gui to generate password for user.

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
+from tkinter import *
+
 import password
 
 
+def button_generate_password():
+    outputEntry.delete(0, END)
+    generated_password = password.Password(entry.get())
+    generated_password.generate_strong_password()
+    generated_password.print_password()
+    outputEntry.insert(0, generated_password.return_password())
+
+
+window = Tk()
+window.geometry("420x420")
+window.title("Password generator")
+
+label = Label(window, text="Entry length of your desired password")
+label.pack()
+entry = Entry(window)
+entry.pack()
+
+button = Button(window, text="Generate password", command=button_generate_password)
+button.pack()
+
+outputLabel = Label(window, text="Your password: ")
+outputLabel.pack()
+outputEntry = Entry(window)
+outputEntry.pack()
+
+window.mainloop()
+
 password_length = input("How long password do you require?\n")
-generated_password = password.Password(password_length)
-generated_password.generate_strong_password()
-generated_password.print_password()
--- password.py
+++ password.py
@@ -8,8 +8,15 @@ class Password:
         self.generated_password = generated_password
 
     def generate_strong_password(self):
-        password_option = string.digits + string.ascii_letters + string.punctuation
-        self.generated_password = ''.join(random.choice(password_option) for i in range(int(self.password_length)))
+        try:
+            password_length = int(self.password_length)
+            password_option = string.digits + string.ascii_letters + string.punctuation
+            self.generated_password = ''.join(random.choice(password_option) for i in range(password_length))
+        except ValueError:
+            print("Please input only a digit as a desired length.")
 
     def print_password(self):
         print(f"Your generated password is: {self.generated_password}")
+
+    def return_password(self):
+        return self.generated_password
